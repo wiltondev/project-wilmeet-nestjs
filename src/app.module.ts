55b40a0e3bda92a1959d9d7c7afd2717a182aaeb
wiltondev <92389600+wiltondev@users.noreply.github.com>
2023-12-11 22:02:14 -0300

    configuração de banco de dados .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
