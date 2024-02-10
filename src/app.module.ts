22fa281d11423f5870c788c82f3bac6fc3532103
wiltondev <92389600+wiltondev@users.noreply.github.com>
2024-02-10 11:00:54 -0300

    guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,23 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports:[
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL), 
     AuthModule,
+    UserModule
 
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide:APP_GUARD,
+      useClass: JwtAuthGuard,
+    }
+  ],
 })
 export class AppModule { }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Post, HttpCode, HttpStatus, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto,  } from "./dtos/login.dto";
 import { RegisterDto } from "src/user/dtos/register.dto";
+import { IsPublic } from "./decorators/ispublic.decorator";
 
 
 
@@ -11,12 +12,14 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
     @Post('login')
     @HttpCode(HttpStatus.OK)
+    @IsPublic()
     login(@Body() dto:LoginDto){
         return this.authService.login(dto)
     }
         
     @Post('register')
     @HttpCode(HttpStatus.OK)
+    @IsPublic()
     register(@Body() dto:RegisterDto){
         return this.authService.register(dto)
     }
